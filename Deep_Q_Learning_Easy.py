8dbb29fb6bc94b5530d3a4dfe6d8c90c6fd97157
ernie0115 <ernie0115@gmail.com>
2025-01-07 11:11:25 +0800

    Add compiling time.

--- Deep_Q_Learning_Easy.py
+++ Deep_Q_Learning_Easy.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim # import QNetwork # import DQNAgent 
 from Environment_Easy import Environment_Easy 
 from train_dqn import train_dqn 
+import time  # Import the time module. 
 
 # Create an instance of your 2D environment. 
 # 351: # of red apples 
@@ -13,5 +14,9 @@ env = Environment_Easy(351, 289)
 
 # Main training loop 
 if __name__ == "__main__": 
+    start_time = time.time()  # Start the timer. 
     num_episodes = 1000 # Adjust the number of episodes as needed. 
     train_dqn(num_episodes, env) 
+    end_time = time.time()  # End the timer. 
+    compiling_time = end_time - start_time  # Calculate compiling time. 
+    print(f"Compiling time: {compiling_time:.4f} seconds")  # Print the compiling time. 
